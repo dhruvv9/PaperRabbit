9a47cf311c1785f23267f429c57c6dd3cd8733dc
shyamg090 <samshyam9630@gmail.com>
2024-03-06 10:35:50 +0530

    navbar popup added-shyam

--- src/components/header/subnavbars/Lounge.jsx
+++ src/components/header/subnavbars/Lounge.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Lounge() {
     return (
-        <div className="w-screen h-[360px] absolute z-50 mt-[400px] flex justify-center items-center gap-4 bg-gray-50 bg-opacity-70 backdrop-blur-2xl">
-            <div className="w-screen h-[360px] absolute bg-gray-50 bg-opacity-70 backdrop-blur-2xl" />
+        <div className=" w-screen top-10 left-0 right-20 fixed h-[360px] z-50 flex justify-center items-center gap-4 bg-gray-50 bg-opacity-70 backdrop-blur-2xl invisible group-hover/item:visible">
+        <div className=" w-screen top-0 right-0 h-[360px] absolute bg-gray-50 bg-opacity-70 backdrop-blur-2xl" />
             <div className='flex justify-start items-start gap-5'>
                 <div className="relative flex flex-col items-start justify-start mx-5">
-                    <div className=" text-neutral-600 text-xs font-light font-['Inter'] leading-none">Services</div>
+                    <div className=" text-neutral-600 text-xs font-light font-['Inter'] leading-none">Longue</div>
                     <div className=" text-black text-2xl font-semibold font-['Inter'] leading-normal mt-[20px] ">Bloge</div>
                     <div className=" text-black text-2xl font-semibold font-['Inter'] leading-normal mt-[16px]">Article</div>
                     <div className=" text-black text-2xl font-semibold font-['Inter'] leading-normal mt-[16px]">CaseStudy</div>
