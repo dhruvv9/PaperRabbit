d3f4145d9a78f2be426e5ecdca37266f1f8f0bcb
shyamg090 <samshyam9630@gmail.com>
2024-03-07 11:09:37 +0530

    viewport changed-2

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,14 +9,14 @@ module.exports = {
       'md': {'min': '768px', 'max': '1024px'},
       // => @media (min-width: 768px and max-width: 1023px) { ... }
 
-      'lg': {'min': '1025px', 'max': '1279px'},
-      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+      'lg': {'min': '1025px'},
+      // // => @media (min-width: 1024px and max-width: 1279px) { ... }
 
-      'xl': {'min': '1280px', 'max': '1535px'},
-      // => @media (min-width: 1280px and max-width: 1535px) { ... }
+      // 'xl': {'min': '1280px', 'max': '1535px'},
+      // // => @media (min-width: 1280px and max-width: 1535px) { ... }
 
-      '2xl': {'min': '1536px'},
-      // => @media (min-width: 1536px) { ... }
+      // '2xl': {'min': '1536px'},
+      // // => @media (min-width: 1536px) { ... }
     },
     extend: {
       fontFamily: {
