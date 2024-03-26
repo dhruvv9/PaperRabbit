18017b26359442fffd9f61b13a1e1ed352adc5c7
dhruvv9 <dhruvakanive4468@gmail.com>
2024-03-26 13:16:32 +0530

    added

--- src/components/navbarlayers/Hutchlayer.jsx
+++ src/components/navbarlayers/Hutchlayer.jsx
@@ -1,9 +1,150 @@
-import React from 'react'
+import React from 'react';
+
+import bannerimg from '../../assets/navbar_layers/Hatch_hero.jpg';
+import service1 from "../../assets/services/services.jpg";
+import service2 from '../../assets/services/services1.jpg';
+import service3 from '../../assets/services/services2.jpg';
+import add from '../../assets/services/add.jpg';
 
 function Hutchlayer() {
-  return (
-   <div></div>
-  )
+    return (
+
+
+        <div className='w-screen h-[700rem] flex flex-items-center relative bg-neutral-100'>
+            <img className="w-[full] h-430px lg:w-full lg:h-476px left-0 top-0 absolute" src={bannerimg} alt='' ></img>
+
+
+
+
+
+            <div className=' grid grid-cols-1 justify-center items-center h-[300rem] w-[1420px] mt-[60rem] ml-[600px] '>
+
+                <div className=" w-[214px] h-14   lg:w-[369px] lg:h-24 ml-[600px]  lg:top-[0]  relative">
+                    <div className="left-0 top-[18.67px]  absolute text-red-200 text-[55.67px] lg:left-0 lg:top-[32px] text-8xl font-normal font-['Heaven']  lg:leading-[64px]">Advertising</div>
+                    <div className="lg:left-[100px] lg:top-0  text-[37.12px] absolute text-black lg:text-[64px] font-normal font-['Revelstoke'] leading-[64px]">Title</div>
+                </div>
+
+
+
+                <div className="w-[406px]   top-[0]  lg:w-[1420px] lg:h-[full]  lg:top-[0rem] relative ">
+
+
+                    <div className=" flex justify-center items-center top-0 text-xl   ml-[600px]   lg:w-[323px]  lg:top-0 relative text-black text-[32px] font-bold font-['Inter'] leading-loose">Why is it important</div>
+                    <div className="w-[406px] h-[300px]   text-xs leading-[18px] lg:w-[1420px] lg:h-[350px]  lg:top-[50px] relative lg:text-justify text-black lg:text-sm font-normal font-['Inter'] lg:leading-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. <br />Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. <br />Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. <br />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. <br />Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. <br />Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. <br />
+                    </div>
+                </div>
+
+
+
+                <div className="w-[406px]   top-[0]  lg:w-[1420px] lg:h-[500px]  lg:top-[0rem] relative ">
+
+
+                    <div className=" flex justify-center items-center top-0 text-xl   ml-[600px]   lg:w-[323px]  lg:top-0 relative text-black text-[32px] font-bold font-['Inter'] leading-loose">Why is it important</div>
+                    <div className="w-[406px] h-[300px]   text-xs leading-[18px] lg:w-[1420px] lg:h-[350px]  lg:top-[50px] relative lg:text-justify text-black lg:text-sm font-normal font-['Inter'] lg:leading-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. <br />Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. <br />Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. <br />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. <br />Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. <br />Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. <br />
+                    </div>
+                </div>
+
+
+                <div className="w-[406px]   top-[0]  lg:w-[1420px] lg:h-[500px]  lg:top-[0rem] relative ">
+
+
+                    <div className=" flex justify-center items-center top-0 text-xl   ml-[600px]   lg:w-[323px]  lg:top-0 relative text-black text-[32px] font-bold font-['Inter'] leading-loose">Why is it important</div>
+                    <div className="w-[406px] h-[300px]   text-xs leading-[18px] lg:w-[1420px] lg:h-[350px]  lg:top-[50px] relative lg:text-justify text-black lg:text-sm font-normal font-['Inter'] lg:leading-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. <br />Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. <br />Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. <br />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. <br />Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. <br />Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. <br />
+                    </div>
+                </div>
+
+
+                <div className="w-[406px]   top-[0]  lg:w-[1420px] lg:h-[500px]  lg:top-[0rem] relative ">
+
+
+                    <div className=" flex justify-center items-center top-0 text-xl   ml-[600px]   lg:w-[323px]  lg:top-0 relative text-black text-[32px] font-bold font-['Inter'] leading-loose">Why is it important</div>
+                    <div className="w-[406px] h-[300px]   text-xs leading-[18px] lg:w-[1420px] lg:h-[350px]  lg:top-[50px] relative lg:text-justify text-black lg:text-sm font-normal font-['Inter'] lg:leading-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. <br />Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. <br />Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. <br />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. <br />Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. <br />Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. <br />
+                    </div>
+                </div>
+
+
+
+                <img className="w-[full ]  top-[0]  lg:w-[1420px] lg:h-[500px]  lg:top-[0rem] relative " src={bannerimg} alt='' />
+
+
+
+
+
+                <div className='w-[full] h-[full] mt-[10rem]'>
+                    <div className=" flex justify-center items-center  relative text-black lg:text-[32px] font-bold font-['Inter'] leading-loose">
+                        Faq</div>
+
+                    <div className="lg:flex lg:justify-center  lg:items-center p-4 text-black lg:text-base text-xs font-normal font-['Inter'] leading-normal">Perspiciatis unde omnis perspiciatis unde omnis perspiciatis und perspiciatis unde omnis perspiciatis und?   </div>
+                    <div className="lg:flex lg:justify-center lg:items-center p-4 text-black lg:text-base text-xs font-normal font-['Inter'] leading-normal">Perspiciatis unde omnis perspiciatis unde omnis perspiciatis und perspiciatis unde omnis perspiciatis und?   </div>
+                    <div className="lg:flex lg:justify-center lg:items-center p-4 text-black lg:text-base text-xs font-normal font-['Inter'] leading-normal">Perspiciatis unde omnis perspiciatis unde omnis perspiciatis und perspiciatis unde omnis perspiciatis und?   </div>
+                    <div className="lg:flex lg:justify-center lg:items-center p-4 text-black lg:text-base text-xs font-normal font-['Inter'] leading-normal">Perspiciatis unde omnis perspiciatis unde omnis perspiciatis und perspiciatis unde omnis perspiciatis und?   </div>
+                    <div className="lg:flex lg:justify-center lg:items-center p-4 text-black lg:text-base text-xs font-normal font-['Inter'] leading-normal">Perspiciatis unde omnis perspiciatis unde omnis perspiciatis und perspiciatis unde omnis perspiciatis und?   </div>
+                    <div className="lg:flex lg:justify-center lg:items-center p-4 text-black lg:text-base text-xs font-normal font-['Inter'] leading-normal">Perspiciatis unde omnis perspiciatis unde omnis perspiciatis und perspiciatis unde omnis perspiciatis und?   </div>
+                    <div className="lg:flex lg:justify-center lg:items-center p-4 text-black lg:text-base text-xs font-normal font-['Inter'] leading-normal">Perspiciatis unde omnis perspiciatis unde omnis perspiciatis und perspiciatis unde omnis perspiciatis und?   </div>
+                    <div className="lg:flex lg:justify-center lg:items-center p-4 text-black lg:text-base text-xs font-normal font-['Inter'] leading-normal">Perspiciatis unde omnis perspiciatis unde omnis perspiciatis und perspiciatis unde omnis perspiciatis und?   </div>
+
+
+                </div>
+
+                   
+                <div className=" flex justify-center items-center  relative text-black lg:text-[32px] mt-[20rem] font-bold font-['Inter'] leading-loose">
+                        Packages</div>
+
+                <div className='grid gap-[30rem] grid-cols-4 w-full items-center mt-[0rem]'>
+
+<div className='w-[450px] h-[876px]  bg-gray-50 bg-opacity-20 rounded-[20px] border border-black backdrop-blur-[20px] relative'>
+
+</div>
+
+<div className='w-[450px] h-[876px]  bg-gray-50 bg-opacity-20 rounded-[20px] border border-black backdrop-blur-[20px] relative'>
+
+</div>
+
+<div className='w-[450px] h-[876px]  bg-gray-50 bg-opacity-20 rounded-[20px] border border-black backdrop-blur-[20px] relative'>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+</div>
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+    )
 }
 
 export default Hutchlayer
+
+
+
+
+
