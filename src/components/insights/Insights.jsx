479f9f5189fe02e84b95c4715496981d9a94a52e
shyamg090 <samshyam9630@gmail.com>
2024-03-26 18:11:08 +0530

    25/3/2024- about us added and animation added using framer motion

--- src/components/insights/Insights.jsx
+++ src/components/insights/Insights.jsx
@@ -36,8 +36,8 @@ function Insights() {
 
                         <div className="text-black dark:text-white  text-xl mac:text-lg md:text-sm sm:text-sm z-20 font-bold font-['Inter'] ">Perspiciatis unde dolori Perspiciatis unde dolori</div>
                         <div className='flex flex-col items-start justify-start mt-2'>
-                            <div className="flex text-justify"><span className="text-black dark:text-white dark:text-white text-xs font-bold font-['Inter'] ">Category - </span><span className="text-black dark:text-white dark:text-white text-xs font-normal font-['Inter'] ">Lorem ipsum</span></div>
-                            <div className="flex text-justify"><span className="text-black dark:text-white dark:text-white text-xs font-bold font-['Inter'] ">Date - </span><span className="text-black dark:text-white dark:text-white text-xs font-normal font-['Inter'] ">DD/MM/YYYY</span></div>
+                            <div className="flex text-justify"><span className="text-black dark:text-white  text-xs font-bold font-['Inter'] ">Category - </span><span className="text-black dark:text-white  text-xs font-normal font-['Inter'] ">Lorem ipsum</span></div>
+                            <div className="flex text-justify"><span className="text-black dark:text-white  text-xs font-bold font-['Inter'] ">Date - </span><span className="text-black dark:text-white  text-xs font-normal font-['Inter'] ">DD/MM/YYYY</span></div>
                         </div>
                         <div className="text-black dark:text-white  text-sm sm:text-xs md:text-xs font-normal font-['Inter'] leading-1 mt-3  h-[115px] overflow-y-scroll pr-1">Lorem Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, minima ex libero ipsa quo impedit quibusdam atque alias vel, nihil aliquam adipisci veritatis odit. Eos rem dignissimos dolor alias iure? ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur officia eum beatae, fuga aliquid similique nisi eligendi. dolor sit amet,  aliqua. Ut enim ad minim veniam.  Ut enim ad minim veniam et dolore magna aliqua. Ut enim ad minim veniam.</div>
                     </div>
--- src/components/navpages/hutch/HutchPageCards2.jsx
+++ src/components/navpages/hutch/HutchPageCards2.jsx
@@ -3,16 +3,17 @@ import services0 from '../../../assets/hutchimg/img.png';
 
 function HutchPageCards2() {
     return (
-        <div className="w-full h-[420px] lg:h-[600px] mac:h-[600px] relative flex flex-col justify-end items-center mt-40">
-            <img className="w-[260px] lg:w-[340px] mac:w-[340px] h-[420px] lg:h-[598px] mac:h-[598px] absolute -z-20 rounded-[20px]" src={services0} alt='' />
+        <div className="w-full h-[460px] lg:h-[600px] relative flex flex-col justify-end items-center mt-40">
+            <img className="w-[280px] lg:w-[340px]  h-[420px] lg:h-[598px]  absolute -z-20 rounded-[20px]" src={services0} alt='' />
 
-            <div className="w-[260px] lg:w-[340px] mac:w-[340px] lg:h-[258px] mac:h-[258px] h-[220px] relative rounded-[20px] flex flex-col justify-center items-center gap-2 group/items ">
-                <div className="w-[260px] lg:w-80 mac:w-[340px] h-[218px] relative rounded-[20px] flex flex-col justify-center items-center gap-2.5 ">
-                    <div className='w-full h-full absolute -z-10 bg-gray-50 bg-opacity-20 rounded-2xl backdrop-blur-[20px]' />
-                    <div class="flex items-center justify-center text-black lg:text-2xl mac:text-2xl font-bold font-['Inter'] leading-loose text-sm ">Perspiciatis unde omrs</div>
+            <div className="w-[280px] lg:w-[340px]  lg:h-[258px]  h-[120px] relative rounded-[20px] flex flex-col justify-center items-center gap-2 group/items ">
+                <div className="w-[280px] lg:w-80  h-[208px] relative rounded-[20px] flex flex-col justify-center items-center gap-2.5 ">
+                    {/* <div className='w-full h-full absolute -z-10 bg-gray-50 bg-opacity-20 rounded-2xl backdrop-blur-[20px]' /> */}
+                    <div class="flex items-center relative justify-center text-black lg:text-2xl mac:text-l font-bold font-['Inter'] leading-loose text-sm ">Perspiciatis unde omrs</div>
                     <div class="text-justify text-black text-xs font-normal relative font-['Inter'] leading-normal p-3">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
                     <div className="w-[122px] h-11 relative">
-                    <button class="w-[122px] lg:h-11 mac:h-11 h-9  bg-zinc-950 rounded-[10px] text-center text-white text-xs md:text-sm font-normal font-['Inter'] leading-none ">Learn</button>            </div>
+                        <button class="w-[122px] lg:h-11 h-9  bg-zinc-950 rounded-[10px] text-center text-white text-xs md:text-sm font-normal font-['Inter'] leading-none ">Learn</button>
+                    </div>
                 </div>
             </div>
         </div>
